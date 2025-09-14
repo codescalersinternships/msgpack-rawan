e1701b9cfd838b338c92b7c0de5b6432da009ba4
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-09-14 12:35:58 +0300

    chore: added usage example in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	msgpack "github.com/codescalersinternships/msgpack-rawan/pkg"
+)
+
 func main() {
-	
+	bytes, err := msgpack.Serialize(true)
+	if err != nil {
+		panic(err)
+	}
+
+	deserialized, _, err := msgpack.Deserialize(bytes)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Deserialized: %v\n", deserialized)
+	fmt.Printf("Type: %T\n", deserialized)
+
 }
